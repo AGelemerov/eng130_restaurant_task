9bf131e3064c7c1368615c49ce06761c89af70af
agelemerov <angel.gelemerov@gmail.com>
2022-10-12 17:10:50 +0100

    finished task

--- task.py
+++ task.py
@@ -7,3 +7,27 @@
 
 # 3
 # As a user, I want to have my order read back to me in formatted way, so I know what I ordered.
+menu = {
+    1: "Pizza",
+    2: "Lasagne",
+    3: "Linguine Carbonara",
+    4: "Fettuccine Alfredo",
+    5: "Risotto",
+    6: "Ravioli"
+}
+amount_of_orders = 0
+cart = []
+
+while amount_of_orders < 3:
+    for item in menu.items():
+        print(str(item[0]) + ".", item[1])
+
+    chosen_item = input("Please choose an item from the menu, 0 to end selection (number): ")
+    if chosen_item == "0":
+        break
+    cart.append(chosen_item)
+    amount_of_orders += 1
+
+print("\nYour order is as follows: ")
+for item in cart:
+    print(item)
